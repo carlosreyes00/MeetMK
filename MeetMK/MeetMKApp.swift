ca918c1e87d951fa07e134a5fb310b65caaf6978
Carlos Reyes <crrmagadan@icloud.com>
2025-05-04 14:50:53 -0400

    Initial Commit

---
+++ MeetMK/MeetMKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMKApp.swift
+//  MeetMK
+//
+//  Created by Carlos Reyes on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetMKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
