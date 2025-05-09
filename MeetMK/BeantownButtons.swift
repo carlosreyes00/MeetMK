6ac126cc5bfafe8a554961f068af91314df9adeb
Carlos Reyes <crrmagadan@icloud.com>
2025-05-09 09:17:59 -0400

    Map was set, and user can search for beaches and playgrounds at the current camera position.

---
+++ MeetMK/BeantownButtons.swift
@@ -0,0 +1,72 @@
+//
+//  BeantownButtons.swift
+//  MeetMK
+//
+//  Created by Carlos Reyes on 5/4/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct BeantownButtons: View {
+    @Binding var position: MapCameraPosition
+    
+    @Binding var searchResults: [MKMapItem]
+    
+    var visibleRegion: MKCoordinateRegion?
+    
+    var body: some View {
+        HStack {
+            Button {
+                search(for: "playground")
+            } label: {
+                Label("Playgrounds", systemImage: "figure.and.child.holdinghands")
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button {
+                search(for: "beach")
+            } label: {
+                Label("Beaches", systemImage: "beach.umbrella")
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button {
+                position = .region(.boston)
+            } label: {
+                Label("Boston", systemImage: "building.2")
+            }
+            .buttonStyle(.bordered)
+            
+            Button {
+                position = .region(.northShore)
+            } label: {
+                Label("North Shore", systemImage: "water.waves")
+            }
+            .buttonStyle(.bordered)
+        }
+        .labelStyle(.iconOnly)
+    }
+    
+    func search(for query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        request.resultTypes = .physicalFeature
+        request.region = visibleRegion ?? MKCoordinateRegion(
+            center: .parking,
+            span: MKCoordinateSpan(latitudeDelta: 0.0125, longitudeDelta: 0.0125))
+        
+        Task {
+            let search = MKLocalSearch(request: request)
+            let response = try? await search.start()
+            searchResults = response?.mapItems ?? []
+            searchResults.forEach { place in
+                print("\(String(describing: place.name))")
+            }
+        }
+    }
+}
+
+//#Preview {
+//    BeantownButtons()
+//}
--- MeetMK/ContentView.swift
+++ MeetMK/ContentView.swift
@@ -6,16 +6,90 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let parking = CLLocationCoordinate2D(
+        latitude: 42.354528,
+        longitude: -71.068369
+    )
+}
+
+extension MKCoordinateRegion {
+    static let boston = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.360256,
+            longitude: -71.057279),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1)
+    )
+        
+    static let northShore = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.547408,
+            longitude: -70.870085),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5)
+    )
+}
 
 struct ContentView: View {
+    
+    @State private var position: MapCameraPosition = .automatic
+    @State private var visibleRegion: MKCoordinateRegion?
+    
+    @State private var searchResults: [MKMapItem] = []
+    
+    @State private var auxText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Map(position: $position) {
+                Annotation("Parking", coordinate: .parking, anchor: .center) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.background)
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.secondary, lineWidth: 5)
+                        Image(systemName: "car")
+                            .padding(5)
+                    }
+                }
+                .annotationTitles(.hidden)
+                
+                ForEach(searchResults, id: \.self) { result in
+                    Marker(item: result)
+                }
+            }
+            .mapStyle(.standard(elevation: .realistic))
+            .safeAreaInset(edge: .bottom) {
+                HStack {
+                    Spacer()
+                    BeantownButtons(position: $position, searchResults: $searchResults, visibleRegion: visibleRegion)
+                        .padding(.top)
+                    Spacer()
+                }
+                .background(.thinMaterial)
+            }
+            .onChange(of: searchResults) {
+                position = .automatic
+            }
+            .onMapCameraChange(frequency: .continuous) { context in
+                visibleRegion = context.region
+                auxText = String(describing: context.region.center)
+            }
+
+            VStack {
+                Text(auxText)
+                .padding()
+                .background(ignoresSafeAreaEdges: .horizontal)
+                .cornerRadius(50)
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
